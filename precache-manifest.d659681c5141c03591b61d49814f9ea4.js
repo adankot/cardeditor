dfb336ffd1f9d4b780d23b7211ba2225a1f74453
tamasadanko <tamas.adanko@guidesmiths.com>
2020-09-15 14:08:35 +0200

    Updates

---
+++ precache-manifest.d659681c5141c03591b61d49814f9ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc19fc9f100fff1b84ee28aee901d029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b93bbdb9b5bc974d0ff",
+    "url": "/static/css/main.a00df7ff.chunk.css"
+  },
+  {
+    "revision": "ecb4f271ffd8850a1613",
+    "url": "/static/js/2.38f79b89.chunk.js"
+  },
+  {
+    "revision": "df9ead2900c74c3c021b73393b2ac3dd",
+    "url": "/static/js/2.38f79b89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b93bbdb9b5bc974d0ff",
+    "url": "/static/js/main.cb82466a.chunk.js"
+  },
+  {
+    "revision": "075316a16c6b8a59e06b",
+    "url": "/static/js/runtime-main.c8f050cf.js"
+  }
+]);
