d84179ce6e331b1ca75eac2becf8e0b45763780b
tamasadanko <tamas.adanko@guidesmiths.com>
2020-09-15 14:11:37 +0200

    Updates

---
+++ precache-manifest.22e874a6c123a45c37ad80e9e9eeeb59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcc845486990d47bcdc24d56d2bd398",
+    "url": "/cardeditor/index.html"
+  },
+  {
+    "revision": "548cb28a39407034b63f",
+    "url": "/cardeditor/static/css/main.a00df7ff.chunk.css"
+  },
+  {
+    "revision": "ecb4f271ffd8850a1613",
+    "url": "/cardeditor/static/js/2.38f79b89.chunk.js"
+  },
+  {
+    "revision": "df9ead2900c74c3c021b73393b2ac3dd",
+    "url": "/cardeditor/static/js/2.38f79b89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "548cb28a39407034b63f",
+    "url": "/cardeditor/static/js/main.10c02898.chunk.js"
+  },
+  {
+    "revision": "636a2432e7956220e7ff",
+    "url": "/cardeditor/static/js/runtime-main.827676e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d659681c5141c03591b61d49814f9ea4.js"
+  "/cardeditor/precache-manifest.22e874a6c123a45c37ad80e9e9eeeb59.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cardeditor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
