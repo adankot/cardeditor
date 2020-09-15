74cbcd06dfb6a98742993df5c56aba6c094287e5
tamasadanko <tamas.adanko@guidesmiths.com>
2020-09-15 14:05:12 +0200

    Dropping material

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,161 +1,137 @@
-import React, { useState } from 'react';
-import { makeStyles, Button, Grid, Container, Paper, Box, TextField } from '@material-ui/core';
+import React from 'react';
+import './Editor.css';
 import html2canvas from 'html2canvas';
 import { saveAs } from 'file-saver';
 
-const useStyles = makeStyles(theme => ({
-  icon: {
-    background: 'green',
-    height: 100,
-    textAlign: 'center',
-    padding: 10,
-  },
-  attack: {
-    background: 'red',
-    height: 100,
-    textAlign: 'center',
-    padding: 10,
-  },
-  health: {
-    background: 'blue',
-    height: 100,
-    textAlign: 'center',
-    padding: 10,
-  },
-  image: {
-    height: 500,
-  },
-  description: {
-    background: 'gray',
-    height: 100,
-    textAlign: 'center',
-    padding: 10,
-  },
-  name: {
-    background: 'gray',
-    height: 100,
-    textAlign: 'center',
-    padding: 10,
-    fontSize: 'vw',
-  },
-  formField: {
-    width: '80%',
-    height: 50,
-    align: 'center',
-    padding: 10,
-  },
-  formText: {
-    width: '100%',
-    height: '100%',
-  }
-}));
+class Editor extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			cost: '',
+			description: '',
+			type: '',
+			image: '',
+			attack: '',
+			health: '',
+			movement: ''
+		};
+		this.cardRef = React.createRef();
+	}
 
-const Editor = () => {
-  console.log(html2canvas);
-  const classes = useStyles();
-  const [name, setName] = useState([]);
-  const [cost, setCost] = useState([]);
-  const [description, setDescription] = useState([]);
-  const [type, setType] = useState([]);
-  const [image, setImage] = useState([]);
-  const [attack, setAttack] = useState([]);
-  const [health, setHealth] = useState([]);
+	render() {
 
-  const onSave = () => {
-    html2canvas(document.getElementById('card')).then(function (canvas) {
-      const newImage = canvas.toDataURL('image/png');
-      saveAs(newImage, `${name}.png`)
-    });
-  };
+		const onSave = () => {
+			html2canvas(this.cardRef.current)
+				.then((canvas) => {
+					console.log('MENTÉS', this.cardRef.current);
+					const newImage = canvas.toDataURL('image/png');
+					return saveAs(newImage, `${this.state.name}.png`);
+				}).catch(err => {
+				console.log('err: ', err);
+			});
+		};
 
-  return (
-    <Container maxWidth="md">
-      <Box my={4}>
-        <Grid container
-              direction='row'
-              justify="center"
-              alignItems="center"
-              spacing={0}
-        >
-          <Grid item xs={6}>
-              <div className={classes.formField}>
-                <TextField id="card-name" label="Name" variant="filled" className={classes.formText}
-                           onChange={event => setName(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-cost" label="Cost" variant="filled" className={classes.formText}
-                           onChange={event => setCost(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-type" label="Type" variant="filled" className={classes.formText}
-                           onChange={event => setType(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-image" label="Image URL" variant="filled" className={classes.formText}
-                           onChange={event => setImage(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-description" label="Description" variant="filled" className={classes.formText}
-                           onChange={event => setDescription(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-attack" label="Attack" variant="filled" className={classes.formText}
-                           onChange={event => setAttack(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <TextField id="card-health" label="Health" variant="filled" className={classes.formText}
-                           onChange={event => setHealth(event.target.value)}/>
-              </div>
-              <div className={classes.formField}>
-                <Button variant="contained" color="primary" id="download" onClick={onSave}>
-                  Save
-                </Button>
-              </div>
-          </Grid>
-          <Grid item xs={6} id="card">
-              <Grid container>
-                <Grid item xs={3} className={classes.icon}>
-                  <div>
-                    {cost}
-                  </div>
-                </Grid>
-                <Grid item xs={6} className={classes.name}>
-                  <div>
-                    {name}
-                  </div>
-                </Grid>
-                <Grid item xs={3} className={classes.icon}>
-                  <div>
-                    {type}
-                  </div>
-                </Grid>
-                <Grid item xs={12} className={classes.image} style={{
-                  backgroundImage: `url(${image})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center center"
-                }}>
-                </Grid>
-                <Grid item xs={3} className={classes.attack}>
-                  <div>
-                    {attack}
-                  </div>
-                </Grid>
-                <Grid item xs={6} className={classes.description}>
-                  <div>
-                    {description}
-                  </div>
-                </Grid>
-                <Grid item xs={3} className={classes.health}>
-                  <div>
-                    {health}
-                  </div>
-                </Grid>
-              </Grid>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
-  );
+		return (
+			<div className="container">
+				<div className="fields-container">
+					<form>
+						<div className="FormField">
+							<label>Name</label>
+							<input id="card-name" className="FormText"
+							       onChange={event => this.setState({ name: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Cost</label>
+							<input id="card-cost" className="FormText"
+							       onChange={event => this.setState({ cost: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Type</label>
+							<input id="card-type" className="FormText"
+							       onChange={event => this.setState({ type: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Image URL</label>
+							<input id="card-image" className="FormText"
+							       onChange={event => this.setState({ image: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Description</label>
+							<input id="card-description" className="FormText"
+							       onChange={event => this.setState({ description: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Attack</label>
+							<input id="card-attack" className="FormText"
+							       onChange={event => this.setState({ attack: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Health</label>
+							<input id="card-health" className="FormText"
+							       onChange={event => this.setState({ health: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<label>Movement</label>
+							<input id="card-movement" className="FormText"
+							       onChange={event => this.setState({ movement: event.target.value })}/>
+						</div>
+						<div className="FormField">
+							<button id="download" onClick={onSave}>
+								Save
+							</button>
+						</div>
+					</form>
+				</div>
+				<div className="card-container" ref={this.cardRef}>
+					<div id="card">
+						<div className="image" id="card-image" style={{
+							backgroundImage: `url(${this.state.image})`,
+							backgroundSize: "cover",
+							backgroundPosition: "center center"
+						}}>
+						</div>
+						<div className="icon">
+							<div>
+								{this.state.cost}
+							</div>
+						</div>
+						<div className="name">
+							<div>
+								{this.state.name}
+							</div>
+						</div>
+						<div className="icon">
+							<div>
+								{this.state.type}
+							</div>
+						</div>
+						<div className="rarity">O</div>
+						<div className="description">
+							<div>
+								{this.state.description}
+							</div>
+						</div>
+						<div className="attack">
+							<div>
+								{this.state.attack}
+							</div>
+						</div>
+						<div className="movement">
+							<div>
+								{this.state.movement}
+							</div>
+						</div>
+						<div className="health">
+							<div>
+								{this.state.health}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	};
 };
 
 export default Editor;
