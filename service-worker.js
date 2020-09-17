c77942a7f92cf3817364b02358f141c04d2bddd3
Márk Matics <mark.matics@ecsolutions.se>
2020-09-17 11:43:22 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cardeditor/precache-manifest.00a7bb1ecb3b2f19da8b101bb029bcae.js"
+  "/cardeditor/precache-manifest.81a28887bb58fd3a9768bd6d08f24f8c.js"
 );
 
 self.addEventListener('message', (event) => {
