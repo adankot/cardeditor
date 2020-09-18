0705fd9e914465e854f4f50f1a055d9513cab56f
Márk Matics <mark.matics@ecsolutions.se>
2020-09-18 14:39:38 +0200

    Updates

---
+++ precache-manifest.e27b50da1fe6ca720e79ba4e0092386c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417c91443b67ba79178a0269beb72c1e",
+    "url": "/cardeditor/index.html"
+  },
+  {
+    "revision": "dcee4f5cadf19e7dbac2",
+    "url": "/cardeditor/static/css/main.285b1575.chunk.css"
+  },
+  {
+    "revision": "4f576269ddc3670e3ca2",
+    "url": "/cardeditor/static/js/2.e31c58fe.chunk.js"
+  },
+  {
+    "revision": "424d464fe208f40ac733f85582d745f0",
+    "url": "/cardeditor/static/js/2.e31c58fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcee4f5cadf19e7dbac2",
+    "url": "/cardeditor/static/js/main.911af610.chunk.js"
+  },
+  {
+    "revision": "636a2432e7956220e7ff",
+    "url": "/cardeditor/static/js/runtime-main.827676e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cardeditor/precache-manifest.81a28887bb58fd3a9768bd6d08f24f8c.js"
+  "/cardeditor/precache-manifest.e27b50da1fe6ca720e79ba4e0092386c.js"
 );
 
 self.addEventListener('message', (event) => {
